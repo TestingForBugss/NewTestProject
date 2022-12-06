45d78687bd20d0f5c8b567b8219ae8a7d23ffbdb
gezginss <omerfarukkiyak51@gmail.com>
2022-12-06 22:59:12 +0300

    Initial commit

---
+++ src/test/java/com/runners/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package com.runners;
+
+
+/*
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/cydeo/step_definitions",
+        features = "@target/rerun.txt"
+)
+ */
+
+public class FailedTestRunner {
+
+}
